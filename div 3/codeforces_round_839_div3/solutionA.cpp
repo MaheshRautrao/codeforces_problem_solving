e6d9c4e04e278c35bf9dcf04eee632c65981fbf6
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-11-25 10:49:36 +0530

    solved a from 839 div 3

--- div 3/codeforces_round_839_div3/solutionA.cpp
+++ div 3/codeforces_round_839_div3/solutionA.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check(int ind, string s, char arr[])
-{
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (s[i] != arr[(ind++) % 3])
-            return false;
-    }
-    return true;
-}
-
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -25,27 +15,9 @@ int main()
     {
         string s;
         cin >> s;
-        string parent = "";
-        for (int i = 0; i < 18; i++)
-            parent += "Yes";
-
-        if (parent.find(s) != string::npos)
-            cout << parent.find(s) << endl;
-        else
-            cout << "NO" << endl;
-        // char arr[3] = {'Y', 'e', 's'};
-        // int flag = 1;
-
-        // if (s[0] == 'Y')
-        //     flag = check(0, s, arr);
-        // else if (s[0] == 'e')
-        //     flag = check(1, s, arr);
-        // else if (s[0] == 's')
-        //     flag = check(2, s, arr);
-        // else
-        //     flag = 0;
-
-        // cout << (flag ? "YES" : "NO") << endl;
+        int first = s[0] - int('0');
+        int second = s[2] - int('0');
+        cout << first + second << endl;
     }
     return 0;
 }
